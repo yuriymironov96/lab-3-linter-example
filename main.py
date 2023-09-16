479245938abdb72d3bd05215af3063f71c9738ce
Yurii Mironov <yuriymironov96@gmail.com>
2023-09-16 16:36:42 +0300

    lint file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def func():
-   a = 'string'
-   b = "string"
-   c = 1+ 1
-   o = 2
-   if True:
-      return None
+    a = "string"
+    b = "string"
+    c = 1 + 1
+    o = 2
+    if True:
+        return None
